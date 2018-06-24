4b1445a438ec0187e84cee5d1aec9353bfd71224
Uli Kusterer <ulivc@zathras.de>
2018-06-24 20:28:00 +0200

    Initial Commit

---
+++ MarlynsWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MarlynsWorld
+//
+//  Created by Uli Kusterer on 24.06.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MarlynsWorld/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MarlynsWorld
+//
+//  Created by Uli Kusterer on 24.06.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
